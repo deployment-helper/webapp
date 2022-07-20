96ae3be27a5e96f4f93be893df97d8c4e3b79417
vinay <vinaymavi@gmail.com>
2022-07-21 00:12:32 +0530

    AWS Amplify added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import './App.css';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({signOut,user}) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign Out</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
